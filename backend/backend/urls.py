55ffacd8d000bb1e5b68c6f13e11a1ff753179ca
Joonghyuk Seong <sjh001111@gmail.com>
2024-08-27 14:31:33 +1000

    learn how to use git

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("accounts.urls"))
+    path('api/', include("accounts.urls")),
     path('api/', include("address.urls")),
     path('api/', include("payment.urls")),
 ]
