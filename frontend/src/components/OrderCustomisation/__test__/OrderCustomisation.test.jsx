60d1c598876f4c5781c19ae7cdd428a707466092
Andy Le <andy.le314@gmail.com>
2024-10-07 00:08:51 +1100

    fixing test T.T

--- frontend/src/components/OrderCustomisation/__test__/OrderCustomisation.test.jsx
+++ frontend/src/components/OrderCustomisation/__test__/OrderCustomisation.test.jsx
@@ -1,9 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import OrderCustomisation from '../OrderCustomisation'
 import Items from '../Items'
+import axios from "axios";
 import "@testing-library/jest-dom";
 
+let mockData = [{"fields": {"option_name": "Secret Sauce", "option_price": "100", "menuItem":[1]},
+                "pk": 1,
+                "model": "custom.option"}]
+
 jest.mock('../Items');
+jest.mock('axios');
+
+axios.request = jest.fn().mockImplementation(() => Promise.resolve({data: mockData}))
+
 const mockedUsedNavigate = jest.fn();
 
 jest.mock('react-router-dom', () => ({
@@ -11,12 +20,19 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
-test('Order Customisation renders', () => {
+test('Order Customisation renders', async () => {
     render(<OrderCustomisation/>)
-    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Order')
+
+    await waitFor(() => {
+        expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Order')
+    })
+    
   })
 
-test('Items was called', () => {
+test('Items was called', async () => {
     render(<OrderCustomisation/>)
-    expect(Items).toHaveBeenCalled()
+
+    await waitFor(() => {
+        expect(Items).toHaveBeenCalled();
+    }) 
 })
